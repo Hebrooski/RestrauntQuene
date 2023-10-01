b3d04fc3a27ceb645425e9c24b1433c6807c80b7
Grayson L <63122082+Hebrooski@users.noreply.github.com>
2023-10-01 17:47:29 -0400

    first commit

---
+++ src/test/java/com/graysonlyv/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graysonlyv.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
