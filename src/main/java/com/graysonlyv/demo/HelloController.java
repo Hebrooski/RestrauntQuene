d27a09f3d40154ded521c514bcc87be73b882a86
Grayson L <63122082+Hebrooski@users.noreply.github.com>
2023-10-01 17:48:58 -0400

    init commit for first spring project

--- src/main/java/com/graysonlyv/demo/HelloController.java
+++ src/main/java/com/graysonlyv/demo/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RestController
 public class HelloController {
 
+	//default route for domain
 	@GetMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
